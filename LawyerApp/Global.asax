8906120c6779d9d9adf060f3e4132f44d4cdd9fd
akmal tharik <mtshakeerakmal@gmail.com>
2019-03-18 15:53:11 +0530

    Add project files.

---
+++ LawyerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LawyerApp.MvcApplication" Language="C#" %>
---
+++ LawyerApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LawyerApp.Startup))]
+namespace LawyerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
